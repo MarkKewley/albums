5ec10b63d55442e3e813c2fa00aab32f94cdbb09
Mark Kewley <markkewley@MarkKewleysiMac.roc.mn.charter.com>
2017-07-02 06:53:17 -0500

    first commit

---
+++ index.ios.js
@@ -0,0 +1,10 @@
+// Place code in here for IOS!
+
+// Import a library to help create a component
+import React from 'react';
+import ReactNative from 'react-native';
+import { AppRegistry } from 'react-native';
+
+// Create a component
+
+// Render it to the device
